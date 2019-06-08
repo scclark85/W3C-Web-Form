302d325123a8ce907f0b07207288fdb5ed7a6a0b
scc85 <sarahchristinec85@gmail.com>
2019-06-07 19:51:56 -0700

    json server/placeholder and w3c update

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 var express = require("express");
-var path = require("path");
-
-// Tells node that we are creating an "express" server
+var bodyParser = require("body-parser");
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+// Sets an initial port
+const port = process.env.PORT || 3000
 
-// Sets up the Express app to handle data parsing
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// application routs 
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
 
-// The below code effectively "starts" our server
-app.listen(PORT, function() {
-  console.log("Web-Form app is listening on PORT: " + PORT);
-});
+app.get("/", (req, res) => {
+})
+
+// app.get("/projects", (req, res) => {
+//   var project1 = { Name: "James", Email: "random@random.com" }
+//   var project2 = { Name: "Beth", Email: "random@random.com" }
+//   res.json([project1, project2])
+//   res.send("hello")
+// })
+
+// app.post("/project_create", (req, res) => {
+//   console.log("creating new project submission...")
+//   res.end()
+// })
+
+// Starts server
+app.listen(port, () => {
+  console.log('JSONPlaceholder listening on http://localhost:' + port)
+})
